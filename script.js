7b25477411353718fafb4994b010b03dc37c22b4
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-02-27 17:24:39 -0500

    added 4 prompts: upper/lower, numbers and special characters

--- script.js
+++ script.js
@@ -3,18 +3,37 @@ var generateBtn = document.querySelector("#generate");
 
 //added a function and variable since there wasn't one
 function generatePassword() {
-  var count = Number(prompt("Please enter the amount of characters you would like in your password.")
+  var count = Number(prompt("Please enter the amount of characters, between 8 - 128, that you would like in your password."));
   console.log(count);
   var letters = "abcdefghijklmnopqrstuvwxyz";
   var genPassword = "";
-for (var i = 0; i < count; i++)
-  gebPassword += letters[Math.floor(Math.random * letters.length)];
+
+  for (var i = 0; i < count; i++) {
+    var random = Math.floor(Math.random() * letters.length);
+    genPassword += letters[random];
+  }
+
+  console.log(genPassword);
+
+  return genPassword;
 }
 
-console.log(genPassword);
+//
+function generatePassword() {
+  var type = prompt("Would you like to include lowercase characters
+  ?");
 
-  return  genPassword;
-}
+  if (type != null) {
+
+  }
+
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var specharactc = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+
+
 
 
 
