4b6b0338b33305f84d85b7a24e93db5f3e383644
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-02-27 14:10:33 -0500

    added generatepassword function and a few variables

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//added a function and variable since there wasn't one
+function generatePassword() {
+  var count = Number(prompt("Please enter the amount of characters you would like in your password.")
+  console.log(count);
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var genPassword = "";
+for (var i = 0; i < count; i++)
+  gebPassword += letters[Math.floor(Math.random * letters.length)];
+}
+
+console.log(genPassword);
+
+  return  genPassword;
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
